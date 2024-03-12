7369feb3fd3f8434b1290200f066a1e24255bcb7
Juan Felipe Sánchez Pérez <juan.sanchez-pe@mail.escuelaing.edu.co>
2024-03-12 16:45:05 -0500

    first commit

---
+++ src/main/java/edu/escuelaing/arem/securespark/HelloWorld.java
@@ -0,0 +1,23 @@
+package edu.escuelaing.arem.securespark;
+
+import static spark.Spark.*;
+/**
+ * Hello world!
+ *
+ */
+public class HelloWorld
+{
+    public static void main( String[] args )
+    {
+        secure("certificados/ecikeystore.p12", "123456", null, null);
+        port(getPort());
+        get("/hello", (req, res) -> "Hello World");
+    }
+
+    static int getPort() {
+        if (System.getenv("PORT") != null) {
+            return Integer.parseInt(System.getenv("PORT"));
+        }
+        return 5000; //returns default port if heroku-port isn't set (i.e. on localhost)
+    }
+}
